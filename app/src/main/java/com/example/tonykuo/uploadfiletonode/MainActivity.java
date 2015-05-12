00b6ca89c6a54fd699bc004430d13dfb360fc2c0
TonyKuo <TonyKuo@TonyKuodeMacBook-Pro.local>
2015-05-12 10:09:16 +0800

    first commit

---
+++ app/src/main/java/com/example/tonykuo/uploadfiletonode/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.tonykuo.uploadfiletonode;
+
+import android.content.ActivityNotFoundException;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.koushikdutta.async.future.Future;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Button imgsel,upload;
+    ImageView img;
+    String path;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = (ImageView)findViewById(R.id.img);
+        Ion.getDefault(this).configure().setLogging("ion-sample", Log.DEBUG);
+        imgsel = (Button)findViewById(R.id.selimg);
+        upload =(Button)findViewById(R.id.uploadimg);
+        upload.setVisibility(View.INVISIBLE);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File f = new File(path);
+
+                Future uploading = Ion.with(MainActivity.this)
+                        .load("http://163.13.127.133:3000/upload")
+                        .setMultipartFile("image", f)
+                        .asString()
+                        .withResponse()
+                        .setCallback(new FutureCallback<Response<String>>() {
+                            @Override
+                            public void onCompleted(Exception e, Response<String> result) {
+                                try {
+                                    JSONObject jobj = new JSONObject(result.getResult());
+                                    Toast.makeText(getApplicationContext(), jobj.getString("response"), Toast.LENGTH_SHORT).show();
+
+                                } catch (JSONException e1) {
+                                    e1.printStackTrace();
+                                }
+
+                            }
+                        });
+            }
+
+        });
+
+        imgsel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fintent = new Intent(Intent.ACTION_GET_CONTENT);
+                fintent.setType("image/jpeg");
+                try {
+                    startActivityForResult(fintent, 100);
+                } catch (ActivityNotFoundException e) {
+
+                }
+            }
+        });
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null)
+            return;
+        switch (requestCode) {
+            case 100:
+                if (resultCode == RESULT_OK) {
+                    path = getPathFromURI(data.getData());
+                    img.setImageURI(data.getData());
+                    upload.setVisibility(View.VISIBLE);
+
+                }
+        }
+    }
+    private String getPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(getApplicationContext(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
